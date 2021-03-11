12f5bcdd2eab6860af437a2d394fef321f466573
alannatran <atran.art@gmail.com>
2021-03-10 19:13:56 -0800

    Google search entries txt file, created random button

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,35 @@
 
+let canvas;
+
+let googlesearchArray;
+let googlesearchButton;
+let randomgooglesearchresultNumber;
+let googlesearchP;
+
+function preload(){
+  googlesearchArray = loadStrings("txtFiles/googlesearchentries.txt");
+}
 
 function setup() {
   // put setup code here to run once
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style("z-index", "-1");
+
+  googlesearchP = createP("");
+  googlesearchP.position(30, 50);
+
+  googlesearchButton = createButton("Random Google Search Entry");
+  googlesearchButton.mousePressed(randgooglesearchresult);
+
 }
 
+function randgooglesearchresult(){
+  randgooglesearchresultNumber = int(random(googlesearchArray.length));
+  googlesearchP.html(googlesearchArray[randgooglesearchresultNumber]);
+}
 
 function draw() {
+  background(255);
   // put drawing code here to run in a loop
-background(20,20,20);
 }
