86f6d59eb2a80dcb53a68155119690f3cf49cb4d
alannatran <atran.art@gmail.com>
2021-03-31 21:07:10 -0700

    Drawing out interface. Added white and gray blocks, text, mock searchbar

--- js/sketch.js
+++ js/sketch.js
@@ -6,6 +6,7 @@ let googlesearchButton;
 let randomgooglesearchresultNumber;
 let googlesearchP;
 
+
 function preload(){
   googlesearchArray = loadStrings("txtFiles/googlesearchentries.txt");
 }
@@ -17,9 +18,9 @@ function setup() {
   canvas.style("z-index", "-1");
 
   googlesearchP = createP("");
-  googlesearchP.position(30, 50);
+  googlesearchP.position(230, 50);
 
-  googlesearchButton = createButton("Random Google Search Entry");
+  googlesearchButton = createButton("Search");
   googlesearchButton.mousePressed(randgooglesearchresult);
 
 }
@@ -31,4 +32,31 @@ function randgooglesearchresult(){
 
 function draw() {
   // put drawing code here to run in a loop
+noStroke();
+fill(207,207,207);
+  rect(0,0, windowWidth, windowHeight);
+
+fill(255,255,255);
+  rect(0,0, windowWidth/2, windowHeight);
+
+strokeWeight(1.5);
+stroke(215);
+fill(255,255,255);
+  rect(windowWidth/9,windowHeight/2,400, 50, 50);
+
+  strokeWeight(3);
+  stroke(207);
+  fill(255,255,255);
+  ellipse(windowWidth/7.8,windowHeight/1.88,20);
+
+noStroke();
+fill(0,0,0);
+textSize(30);
+text('Personality Categorizer', windowWidth/7, windowHeight/2.4);
+
+textSize(20);
+text('Categorizing the personality of', windowWidth/1.5, windowHeight/7);
+
+textSize(30);
+text('Alanna Tran', windowWidth/1.43, windowHeight/5);
 }
